e81f4221c8bc22c173972698a6d5256bd7996128
McKay Knell <mckay@devpipeline.com>
2024-02-16 16:32:40 -0700

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,56 @@
+//1:
+// const objOne = {
+//   id: 1,
+//   name: "Ryan",
+// };
+
+// const objTwo = {
+//   address: "123 fake st",
+//   phone: "555-555-5555",
+// };
+// arraOne = ["123 fake st", "555-555-5555"];
+// arrayTwo = [1, "Ryan"];
+
+// const objFinal = { ...objOne, ...objTwo };
+
+// const arrayFinal = { ...objOne, ...objTwo };
+// console.log(arrayFinal);
+
+//2
+
+// const numOne = 9;
+// const numTwo = 12;
+
+// const ternary =
+//   numOne > numTwo
+//     ? "numOne is the largest number"
+//     : numTwo > numOne
+//     ? "numTwo is the largest number"
+//     : "these numbers are equal";
+// console.log(ternary);
+
+//3
+
+// const string = "I am a sentence";
+
+// const array = string.split(" ");
+
+// const joinSentence = [];
+
+// for (let item of array) {
+//   joinSentence.push(item[0].toUpperCase() + item.slice(1));
+// }
+
+// console.log(joinSentence.join(" "));
+
+//4
+
+// const string = "Hi there";
+
+// const capitalize = string.toUpperCase();
+
+// const array = capitalize.split("");
+
+// const result = array.sort();
+
+// console.log(result.join(" "));
